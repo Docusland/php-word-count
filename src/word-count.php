1b3d4797fe437beb19b83c79f70cd037a7464e2e
Docusland <erwann.duclos@gmail.com>
2020-03-06 08:02:42 +0100

    Initial Commit

---
+++ src/word-count.php
@@ -0,0 +1,13 @@
+<?php
+
+//
+// This is only a SKELETON file for the "Hello World" exercise.
+// It's been provided as a convenience to get you started writing code faster.
+//
+
+function wordCount($phrase)
+{
+    //
+    // YOUR CODE GOES HERE
+    //
+}
